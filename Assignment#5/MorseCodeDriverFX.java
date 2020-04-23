a2860dd99b951d080b8a6b6af125ba7c725109f9
Oumar <54870704+oumard400@users.noreply.github.com>
2020-04-22 21:22:16 -0400

    Add files via upload

---
+++ Assignment#5/MorseCodeDriverFX.java
@@ -0,0 +1,36 @@
+package Data;
+
+import javafx.application.Application;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("unused")
+public class MorseCodeDriverFX extends Application{
+	
+   
+   public static void main(String[] args){
+	   launch(args);
+   }
+   
+   public void start(Stage stage)
+   {
+
+	   //call the main scene which is a BorderPane
+	   MorseCodeMain mainPane = new MorseCodeMain();
+	   //PasswordMain root = mainPane.getTopContainer();
+	   Scene scene = new Scene(mainPane, 550, 350);
+	   stage.setScene(scene);
+	   stage.setTitle("Morse Code Converter Driver");
+	   stage.show();
+   }
+   
+}
